3abf0f145d1b13895e5014b847cfeddb59203649
nefren <woytekk12@gmail.com>
2021-03-16 16:17:51 +0100

    add new item done

--- App/Controllers/Panel.php
+++ App/Controllers/Panel.php
@@ -18,15 +18,22 @@ class Panel extends \Core\Controller {
         $val = new Validation();
         $data = [];
 
-        $data['link'] = $val->test_input($_POST['link']);
-        $data['name'] = $val->test_input($_POST['name']);
-        
+        if ($val->name('link')->value($_POST['link'])->required()) {
+            $data['link'] = $val->test_input($_POST['link']);
+        }
+
+        if ($val->name('name')->value($_POST['name'])->required()) {
+            $data['name'] = $val->test_input($_POST['name']);
+        }
+
+        if ($val->name('tag')->value($_POST['tag'])->required()) {
+            $data['tag'] = $val->test_input($_POST['tag']);
+        }
 
         if ($val->isSuccess()) {
             try {
                 $newLink = new Add('links');
                 $add = $newLink->AddData($data);
-
             } catch (Exception $e) {
                 echo 'Caught exception: ',  $e->getMessage(), "\n";
             } 
@@ -42,14 +49,19 @@ class Panel extends \Core\Controller {
 
         $show = new Display('links');
         $showLinks = $show->getAllData();
+        $send = false;
 
         if (isset($_POST['submit'])) {
             $this->addLink();
             $showLinks = $show->getAllData();
+            $send = true;
+        } else {
+            $send = false;
         }
 
         View::renderTemplate('home/panel.html', [
-            'showLinks' => $showLinks
+            'showLinks' => $showLinks,
+            'send' => $send
         ]); 
     }
 }
--- public/static/js/scripts.js
+++ public/static/js/scripts.js
@@ -1,1 +1,15 @@
-console.log('ok');
+(function () {
+    'use strict'
+    let forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
